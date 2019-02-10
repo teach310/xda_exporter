62bcefd39916242eafdc6b6b15b57af25abe36e8
Taichi Sato <okms310gmail.com>
2019-02-11 08:36:22 +0900

    feat save multiple

--- textWriter.js
+++ textWriter.js
@@ -1,7 +1,7 @@
 const fs = require("uxp").storage.localFileSystem;
 
-
 const saveText = async (suggestedName, text) => {
+    suggestedName = verifyString(suggestedName);
     const file = await fs.getFileForSaving(suggestedName);
     if (!file) {
         // file picker was cancelled
@@ -10,6 +10,23 @@ const saveText = async (suggestedName, text) => {
     await file.write(text);
 }
 
+const saveTexts = async (fileInfos) => {
+    const folder = await fs.getFolder();
+    if (!folder) {
+        // folder picker was cancelled
+        return;
+    }
+    for (var t of fileInfos){
+        const file = await folder.createFile(verifyString(t.name));
+        await file.write(t.text);
+    };
+}
+
+const verifyString = (text) => {
+    return text.replace(/\//g, "-");;
+}
+
 module.exports = {
-    saveText
+    saveText,
+    saveTexts
 }
